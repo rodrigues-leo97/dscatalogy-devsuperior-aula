70021d9d13e82e2d46ad9a2915da011a0c5bf1a3
Leonardo Rodrigues <leonardodrodriguess@gmail.com>
2022-05-19 23:17:49 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 
@@ -21,9 +22,9 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<CategoryDTO>> findAll() { //ResponseEntity vai encapsular uma resposta HTTP
+		List<CategoryDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list); //ok() retorna quando uma requisição é do código 200(deu certo)
 		
 	}
 
