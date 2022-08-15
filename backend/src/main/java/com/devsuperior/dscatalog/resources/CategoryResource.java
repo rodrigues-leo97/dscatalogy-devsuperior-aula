40d17c42c3f4505d70735d81e10e613148a89310
Leonardo Rodrigues <leonardodrodriguess@gmail.com>
2022-08-15 15:49:35 -0300

    Refactor Pageable

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,19 +34,9 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "DESC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			//@RequestParam -> o parâmetro é opcional, ou seja, não é obrigatório passar
-
-			) { //ResponseEntity vai encapsular uma resposta HTTP
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) { //ResponseEntity vai encapsular uma resposta HTTP
 		
-		//verificar
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list); //ok() retorna quando uma requisição é do código 200(deu certo)
 	}
 	
--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -6,6 +6,7 @@ import com.devsuperior.dscatalog.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,19 +23,19 @@ public class ProductResource {
 	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findAll(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "DESC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			//@RequestParam -> o parâmetro é opcional, ou seja, não é obrigatório passar
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) { //ResponseEntity vai encapsular uma resposta HTTP
+		//PARAMETRO -> PAGE, SIZE SORT: só mudar no postman a forma como faz a chamada HTTP na url, ex: (/products?page=0&size=12&sort=name,asc) o asc é de ascendente
 
-			) { //ResponseEntity vai encapsular uma resposta HTTP
+		//@RequestParam(value = "page", defaultValue = "0") Integer page,
+		//@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+		//@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+		//@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+		//@RequestParam -> o parâmetro é opcional, ou seja, não é obrigatório passar
+		//RETIRANDO TODOS OS PARAMETROS PASSADOS NA MÃO E COLOCANDO O OBJETO PAGEABLE NO PARAMETRO
+
+		//PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
-		//verificar
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		Page<ProductDTO> list = service.findAllPaged(pageRequest);
+		Page<ProductDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list); //ok() retorna quando uma requisição é do código 200(deu certo)
 	}
 	
