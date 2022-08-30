1e4f013f5ef4babfb96535a14a14f4a58c1a60ae
Leonardo Rodrigues <leonardodrodriguess@gmail.com>
2022-08-29 22:17:05 -0300

    feat: implementação dos testes na camada CONTROLLER/RESOURCE

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -12,14 +12,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.net.URI;
 import java.util.List;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest
+@WebMvcTest(ProductResource.class)
 public class ProductResourceTests { //testes na CAMADA DE WEB(TESTE DE UNIDADE) -> carrega o contexto, porém, somente da camada web(teste unidade: CONTROLLER)
 
     //vamos chamar os ENDPOINTS, testar as REQUISIÇÕES
@@ -45,7 +50,14 @@ public class ProductResourceTests { //testes na CAMADA DE WEB(TESTE DE UNIDADE) 
     }
 
     @Test
-    public void findAllShouldReturnPage() throws Exception{
-        //mockMvc.perform(get("/products"));
+    public void findAllShouldReturnPage() throws Exception {
+        ResultActions result =
+                mockMvc.perform(get("/products")
+                        .accept(MediaType.APPLICATION_JSON) //mediaType = essa requisição vai aceitar como resposta o tipo Json
+                );
+
+        //dividir e colocar em uma variável como está acima, para fazer em baixo como se fosse a ASSERTIONS
+        result.andExpect(status().isOk());
     }
 }
+
