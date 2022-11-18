54b5b66b2bf78b186aaf6d3c1abf9bfee317cf05
Leonardo Rodrigues <leonardodrodriguess@gmail.com>
2022-11-17 23:16:01 -0300

    Informações adicionais no TOKEN

--- backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration //por se tratar de uma classe de CONFIGURAÇÃO
 public class AppConfig {
+
+    @Value("${jwt.secret") //com essa anotação ele pega o valor da variável que está definido no application.properties
+    private String jwtSecret;
 
     @Bean //é uma annotation de MÉTODO, e dizendo que está instância é um componente gerenciado pelo SPRING e posso instancialo em outro componente
     public BCryptPasswordEncoder passwordEncoder() {
@@ -18,7 +22,7 @@ public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-JWT-SECRET"); //por enquanto a assinatura irá ficar hardcode aqui, depois iremos colocar ela no PROPERTIES(Serve para registrar a chave do token)
+        tokenConverter.setSigningKey(jwtSecret); //por enquanto a assinatura irá ficar hardcode aqui, depois iremos colocar ela no PROPERTIES(Serve para registrar a chave do token)
         return tokenConverter;
     }
 
--- backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscatalog.config;
 
+import com.devsuperior.dscatalog.components.JwtTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,8 +11,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer //serve para realizar processamento informando que essa classe é quem representa o AuthorizationServe do OAuth
@@ -28,6 +34,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    //com essa anotação ele pega o valor da variável que está definido no application.properties
+    @Value("${jwt.duration}")
+    private int jwtDuration;
+
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+
+    @Autowired
+    private JwtTokenEnhancer tokenEnhancer;
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -36,18 +55,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("dscatalog") //esperando que diga o nome da aplicação(id dela), na aplicação ao acessar terá que informar o nome
-                .secret(bCryptPasswordEncoder.encode("dscatalog123")) //para definir o clientSecret em hardcode mesmo
+                .withClient(clientId) //esperando que diga o nome da aplicação(id dela), na aplicação ao acessar terá que informar o nome
+                .secret(bCryptPasswordEncoder.encode(clientSecret)) //para definir o clientSecret em hardcode mesmo
                 .scopes("read", "write") //para informar que é um acesso de leitura e escrita
                 .authorizedGrantTypes("password")//os tipos de acesso de login, nesse caso é password
-                .accessTokenValiditySeconds(86400); //para informar o tempo de expiração do token, no caso 24h
+                .accessTokenValiditySeconds(jwtDuration); //para informar o tempo de expiração do token, no caso 24h
     }
 
     //configuração para saber quem vai autorizar e qual o formato do token
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain chain = new TokenEnhancerChain();
+        chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer)); //esse argumento espera uma lista
+
         endpoints.authenticationManager(authenticationManager) //nesse caso será o authentication que processará a autenticação
                 .tokenStore(tokenStore) //objetos responsáveis por processar o TOKEN
-                .accessTokenConverter(accessTokenConverter); //
+                .accessTokenConverter(accessTokenConverter)
+                .tokenEnhancer(chain);
     }
 }
