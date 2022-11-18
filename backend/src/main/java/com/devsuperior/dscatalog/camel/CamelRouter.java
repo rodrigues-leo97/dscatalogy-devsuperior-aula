500ed1a72852056b69bc4a6a36ef6e2a4a3faba8
Leonardo Rodrigues <leonardodrodriguess@gmail.com>
2022-11-17 22:40:08 -0300

    criação da branch com lib do CAMEL

---
+++ backend/src/main/java/com/devsuperior/dscatalog/camel/CamelRouter.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscatalog.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class CamelRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
