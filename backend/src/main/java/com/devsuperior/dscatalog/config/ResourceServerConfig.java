e1e98b3a62ec6d7c9ed3c2d195725f37e2ad2fa8
Leonardo Rodrigues <leonardodrodriguess@gmail.com>
2022-11-23 20:39:26 -0300

    configs para liberar h2(perfil de teste)

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,14 +10,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer //processa para que a classe implemente a funcionalidade do ResourceServe
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment env; //a partir dele dá pra acessar diversas variáveis
+
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"}; //endpoints publicos
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"}; //endpoints publicos
 
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"}; //rotas que estarão liberadas para quem for operador e admin
 
@@ -30,6 +36,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception { //configuração das rotas e definir as configurações de acesso
+
+        //para testar os profiles de execução que estou rodando (H2)
+        //vou converter isso para uma lista
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) { //se nos PROFILES ativos eu tenho um perfil de test
+            http.headers().frameOptions().disable(); //interface do h2 requer que desabilite essas questões de proteger os frames para que ela pegue
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll() //antMatchers -> não exige login nessas rotas e permite tudo relacionado a elas
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() //estou dizendo que é pra liberar para todo mundo SOMENTE para o método GET
